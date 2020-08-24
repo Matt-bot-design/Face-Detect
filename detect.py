d30b77c20a4a3e1b76327c043c269f217fddca91
Matt-bot-design <62379991+Matt-bot-design@users.noreply.github.com>
2020-08-24 15:16:32 +0200

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
+rec = cv2.face.LBPHFaceRecognizer_create()
+rec.read('trainner.yml')
+
+vid = cv2.VideoCapture(0)
+
+while (True):
+    cap, frame = vid.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        #print(x, y, w, h)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        id_, conf = rec.predict(roi_gray)
+        if conf>=45 and conf<=85:
+            print(id_)
+
+        img_item = "me-image.png"
+        cv2.imwrite(img_item, roi_gray)
+
+        color = (255, 100, 55)
+        stroke = 5
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+    cv2.imshow('frame', frame)
+    key = cv2.waitKey(20)
+    if key == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
